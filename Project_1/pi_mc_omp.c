1587ff44868d85ce68ad4622ffe4a84fd84bcdcf
Matt Wilchek <mwilchek@gmu.edu>
2019-03-17 23:16:13 -0400

    Add files via upload

--- Project_1/pi_mc_omp.c
+++ Project_1/pi_mc_omp.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <omp.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 static long samples = 100000000;
 
@@ -13,14 +13,17 @@ int main (int argc, char *argv[])
 		exit(99);
 	}
 	long i;  long hits = 0;
-	double pi, x, y, test;
+	double pi, x, y, test, moment;
 	double r = 1.0;   // radius of circle. Side of square is 2*r 
-	srand(atoi(argv[1])* (unsigned)time(NULL));
-
+	
+	moment = omp_get_wtime();
+        unsigned seed = atoi(argv[1])* (unsigned)time(NULL);
+	 
+#pragma omp parallel for private(x, y, test) firstprivate(seed) reduction(+:hits) schedule(static, 2000)
 	for(i=0;i<samples; i++)
 	{
-		x = ( (double)rand())/( (double)RAND_MAX);
-		y = ( (double)rand())/( (double)RAND_MAX);
+		x = ( (double)rand_r(&seed))/( (double)RAND_MAX);
+		y = ( (double)rand_r(&seed))/( (double)RAND_MAX);
 
 		test = x*x + y*y;
 
@@ -30,7 +33,7 @@ int main (int argc, char *argv[])
 	pi = 4.0 * ((double)hits/(double)samples);
 
 	printf("\n %ld trials, pi is %f \n",samples, pi);
-
+	printf(" in %f seconds\n", omp_get_wtime()-moment);
 	return 0;
 }
 
